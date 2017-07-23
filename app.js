e35b63f0ecd09329abc21c3b61a15d753256241d
Spencer <spencer.bartonfisher@me.com>
2017-07-23 14:05:13 +0100

    WIP: Adds signup and login pages with routing

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
+var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 var Message = require('./models/message');
+
 // mongoose.connect('mongodb://localhost/babelchat_test');
 
 var app = require('express')();
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -20,9 +47,6 @@ mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
-app.get('/sign_up', function(req, res){
-  res.sendFile('/sign_up.ejs');
-});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-// 'use strict'
-//
-// var express = require('express');
-// var router = express.Router();
-//
-//
-// router.post('/sign_up', function(req, res, next) {
-//   var credentials = {'username': req.body.username}, 'password': req.body.password };
-// });
